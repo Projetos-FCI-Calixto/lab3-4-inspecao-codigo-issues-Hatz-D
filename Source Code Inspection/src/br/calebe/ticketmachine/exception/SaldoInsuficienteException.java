e924b4025ba26d778a748b15e895b89004563051
Diogo Hatz <124005927+Hatz-D@users.noreply.github.com>
2025-08-28 21:26:49 -0300

    Add files via upload

--- Source Code Inspection/src/br/calebe/ticketmachine/exception/SaldoInsuficienteException.java
+++ Source Code Inspection/src/br/calebe/ticketmachine/exception/SaldoInsuficienteException.java
@@ -1,3 +1,5 @@
+// Diogo Lourenzon Hatz - 10402406
+
 package br.calebe.ticketmachine.exception;
 
 /**
@@ -5,4 +7,5 @@ package br.calebe.ticketmachine.exception;
  * @author Calebe de Paula Bianchini
  */
 public class SaldoInsuficienteException extends Exception {
+        // Defeito por comissão: Somente a classe de Exception foi criada, mas não há a presença de nenhum método de exceção ou construtor.
 }
